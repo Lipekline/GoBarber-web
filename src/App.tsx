0043c958ae05c6361f304a1c66982d749b0e403b
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-09-28 15:24:42 -0300

    Implementa Layout da Pagina de Login.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import SignIn from "./pages/SignIn";
+import GlobalStyle from "./styles/global";
 
 // import { Container } from './styles';
 
-const App: React.FC = () => {
-  return <h1>Hello World</h1>;
-};
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
