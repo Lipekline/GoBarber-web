36131275495cc538e910a193ee9f169572f036d3
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-09-29 11:58:35 -0300

    Validando Cadastro.

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback } from "react";
+import { Container, Content, Background } from "./styles";
+import logoImg from "../../assets/logo.svg";
+import { FiArrowLeft, FiMail, FiUser, FiLock } from "react-icons/fi";
+import { Form } from "@unform/web";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+import * as Yup from "yup";
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome obrigatório"),
+        email: Yup.string()
+          .required("E-mail obrigatório")
+          .email("Digite um E-mail válido"),
+        password: Yup.string().min(6, "No minimo 6 digitos"),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="email" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
